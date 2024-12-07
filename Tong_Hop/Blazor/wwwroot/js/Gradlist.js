b428436ed6124d3719156582c8f19a2d1aa9352e
minhquang11061998-gif <ninhminhquang110698@gmail.com>
2024-12-07 17:33:48 +0700

    quang

---
+++ Tong_Hop/Blazor/wwwroot/js/Gradlist.js
@@ -0,0 +1,19 @@
+﻿    $(document).on('contextmenu', '.target', function (e) {
+        e.preventDefault();
+        $('.context-menu').hide();
+        $(this).siblings('.context-menu').css({
+            display: 'block',
+            left: e.pageX,
+            top: e.pageY
+        });
+    });
+    $(document).on('click', function () {
+        $('.context-menu').hide();
+    });
+    function downloadFile(fileName, base64String) {
+        const link = document.createElement('a');
+        link.href = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + base64String;
+        link.download = fileName;
+        link.click();
+    };
+
