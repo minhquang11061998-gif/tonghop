a8ee37390dc1f0d91edb7b9abcb1a6ebb5e41d15
Khai-code <quangkhai2004bv@gmail.com>
2025-01-01 22:30:14 +0700

    20250101223100

--- Tong_Hop/DataBase/Models/Tests.cs
+++ Tong_Hop/DataBase/Models/Tests.cs
@@ -15,7 +15,7 @@ namespace DataBase.Models
         public Guid Id { get; set; }
         [StringLength(30, ErrorMessage = "Tên bài kiểm tả không quá 30 ký tự")]
         public string Name { get; set; }
-        public int Code { get; set; }
+        public string Code { get; set; }
         public int? Minute { get; set; }
         public DateTime CreationTime { get; set; }
         public int Status { get; set; }
