fb3beb87c87de1c85fb28a5f9d158cf10b04884c
minhquang11061998-gif <ninhminhquang110698@gmail.com>
2025-01-10 10:29:32 +0700

    quang update 10/1/2025

---
+++ Tong_Hop/Blazor/wwwroot/js/Animation.js
@@ -0,0 +1,10 @@
+﻿function loadLottieFromJson(elementId, jsonUrl) {
+    var animationContainer = document.getElementById(elementId);
+    var animation = lottie.loadAnimation({
+        container: animationContainer,
+        renderer: 'svg',  // Sử dụng renderer SVG
+        loop: true,       // Lặp vô hạn
+        autoplay: true,   // Tự động phát
+        path: jsonUrl     // Đường dẫn tới tệp JSON
+    });
+}
